43cc43f76b1e742b6ba98fc853377c137930cb47
tvnr6900 <venkyjob452@gmail.com>
2019-05-08 20:24:16 +0530

    commit

--- include/dbcon.php
+++ include/dbcon.php
@@ -3,16 +3,16 @@
  class Dbconnection{
     private $dsn = "mysql:host=localhost;dbname=myDatabase;charset=utf8mb4";
     private $options = array(
-      PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
-      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC //make the default fetch be an associative array
+      PDO::ATTR_EMULATE_PREPARES   => false,
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC 
     );
     try {
       $pdo = new PDO($dsn, "username", "password", $options);
-    } catch (Exception $e) {
+    } catch (PDOException $e) {
       error_log($e->getMessage());
       exit('Something weird happened'); //something a user can understand
     }
  }
 
-?>
+?> 
