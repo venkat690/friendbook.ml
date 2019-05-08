2c07bd9aa68808ec7701ec95a7f5aebbde4094de
tvnr6900 <venkyjob452@gmail.com>
2019-05-08 20:48:23 +0530

    commit

---
+++ include/connection.php
@@ -0,0 +1,25 @@
+<?php
+Class Connection {
+    private  $server = "mysql:host=localhost;dbname=cloudways";
+    private  $user = "root";
+    private  $pass = "";
+    private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                              PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                              PDO::ATTR_EMULATE_PREPARES   => false
+                            );
+    protected $con;
+    public function openConnection(){
+        try{
+            $this->con = new PDO($this->server, $this->user,$this->pass,$this->options);
+            return $this->con;
+          }
+          catch (PDOException $e){
+            echo "There is some problem in connection: " . $e->getMessage();
+          }
+    }
+    public function closeConnection(){
+      $this->con = null;
+    }
+}
+
+?>
