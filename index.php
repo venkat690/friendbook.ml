47780f4671f167d98cf13091077cf949a72ebee1
tvnr6900 <venkyjob452@gmail.com>
2019-05-08 23:10:57 +0530

    images

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
-
 <?php 
 include 'include/html_codes.php';
 way_top();
 css();
 ?>
+ <!-- Font Icon -->
+ <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
 
+<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
 </head> 
 <body>
 <?php
     navbar_before();
 ?>
+<section style="background-image: url('images/slider-bg-01.jpg')">
 
+
+
+
+</section>
+<?php js(); ?>
+<script src="js/main.js"></script>
+    </body>
+</html>
